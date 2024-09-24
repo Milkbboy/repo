7b00c41a4cc62fcb39b96880af7132e17d8b0f59
Milkbboy <okayly@hotmail.com>
2024-09-25 08:48:16 +0900

    로비 - 마스터 선택 적용
    - 마스터 선택하고 Act 씬으로 이동

--- Assets/ERang/Scripts/Lobby.cs
+++ Assets/ERang/Scripts/Lobby.cs
@@ -11,28 +11,15 @@ namespace ERang
     {
         public Transform parentMasterPrefab;
         public GameObject masterPrefab;
-        public float spacing = 2f;
         public ToggleGroup toggleGroup; // ToggleGroup 추가
-
         public int selectedMasterId = 0;
 
+        private readonly float spacing = 2f;
         private NextScene nextScene;
 
         void Awake()
         {
             nextScene = GetComponent<NextScene>();
-        }
-
-        public void NextScene()
-        {
-            if (selectedMasterId == 0)
-                return;
-
-            // 선택된 MasterId를 PlayerPrefs에 저장
-            PlayerPrefs.SetInt("SelectedMasterId", selectedMasterId);
-            PlayerPrefs.Save();
-
-            nextScene.Play();
         }
 
         // Start is called before the first frame update
@@ -51,106 +38,81 @@ namespace ERang
             for (int i = 0; i < masterDatas.Count; i++)
             {
                 Vector3 position = new(startX + i * (prefabWidth + spacing), 0, 0);
-                // GameObject instance = Instantiate(masterPrefab, position, Quaternion.identity, parentMasterPrefab);
-                GameObject instance = Instantiate(masterPrefab);
-                instance.transform.SetParent(parentMasterPrefab, false); // 부모 설정
-                instance.transform.localPosition = position; // 부모의 로컬 좌표계에서 위치 설정
+
+                GameObject masterObject = Instantiate(masterPrefab, position, Quaternion.identity, parentMasterPrefab);
+                masterObject.transform.localPosition = position; // 부모의 로컬 좌표계에서 위치 설정
 
                 // 자식 오브젝트인 Portrait를 찾아 Image 컴포넌트 설정
-                Transform portraitTransform = instance.transform.Find("Portrait");
-                if (portraitTransform != null)
+                Transform portraitTransform = masterObject.transform.Find("Portrait");
+
+                if (portraitTransform == null)
+                    continue;
+
+                if (!portraitTransform.TryGetComponent<Image>(out var portraitImage))
+                    continue;
+
+                // Texture2D를 Sprite로 변환하여 설정
+                Texture2D masterTexture = masterDatas[i].masterTexture;
+
+                if (masterTexture != null)
                 {
-                    Image portraitImage = portraitTransform.GetComponent<Image>();
-                    if (portraitImage != null)
-                    {
-                        // Texture2D를 Sprite로 변환하여 설정
-                        Texture2D masterTexture = masterDatas[i].masterTexture;
-                        if (masterTexture != null)
-                        {
-                            Sprite masterSprite = Sprite.Create(
-                                masterTexture,
-                                new Rect(0, 0, masterTexture.width, masterTexture.height),
-                                new Vector2(0.5f, 0.5f)
-                            );
-                            portraitImage.sprite = masterSprite; // masterTexture로 변경
-                        }
+                    Sprite masterSprite = Sprite.Create(
+                        masterTexture,
+                        new Rect(0, 0, masterTexture.width, masterTexture.height),
+                        new Vector2(0.5f, 0.5f)
+                    );
 
-                        // Toggle 컴포넌트를 Portrait에 추가
-                        Toggle toggleButton = portraitTransform.gameObject.AddComponent<Toggle>();
-                        toggleButton.group = toggleGroup;
-                        toggleButton.targetGraphic = portraitImage;
-
-                        // 선택된 상태를 표시하기 위해 색상 변경
-                        ColorBlock colors = toggleButton.colors;
-                        colors.normalColor = Color.white;
-                        colors.selectedColor = Color.green; // 선택된 상태의 색상
-                        toggleButton.colors = colors;
-
-                        // 버튼 클릭 이벤트 설정
-                        int masterId = masterDatas[i].master_Id; // 클로저 문제를 피하기 위해 로컬 변수 사용
-                        toggleButton.onValueChanged.AddListener((isSelected) => ToggleMaster(masterId, isSelected));
-
-                        // Toggle 버튼의 라벨에 캐릭터 이름 설정
-                        Transform labelTransform = portraitTransform.Find("Label");
-                        if (labelTransform != null)
-                        {
-                            Text labelText = labelTransform.GetComponent<Text>();
-                            if (labelText != null)
-                            {
-                                labelText.text = masterId.ToString(); // 캐릭터 이름 설정
-                            }
-                        }
-                    }
+                    portraitImage.sprite = masterSprite; // masterTexture로 변경
                 }
 
-                // // 자식 오브젝트인 ToggleButton을 찾아 Toggle 컴포넌트 설정
-                // Transform toggleButtonTransform = instance.transform.Find("Toggle");
+                // Toggle 컴포넌트를 Portrait에 추가
+                Toggle toggleButton = portraitTransform.gameObject.AddComponent<Toggle>();
+                toggleButton.group = toggleGroup;
+                toggleButton.targetGraphic = portraitImage;
 
-                // if (toggleButtonTransform != null)
-                // {
-                //     Toggle toggleButton = toggleButtonTransform.GetComponent<Toggle>();
-                //     if (toggleButton != null)
-                //     {
-                //         // ToggleGroup에 추가
-                //         toggleButton.group = toggleGroup;
+                // 선택된 상태를 표시하기 위해 색상 변경
+                ColorBlock colors = toggleButton.colors;
+                colors.normalColor = Color.white;
+                colors.selectedColor = Color.green; // 선택된 상태의 색상
+                toggleButton.colors = colors;
 
-                //         // 버튼 클릭 이벤트 설정
-                //         int index = i; // 클로저 문제를 피하기 위해 로컬 변수 사용
-                //         toggleButton.onValueChanged.AddListener((isSelected) => ToggleMaster(index, isSelected));
+                // 버튼 클릭 이벤트 설정
+                int masterId = masterDatas[i].master_Id; // 클로저 문제를 피하기 위해 로컬 변수 사용
+                toggleButton.onValueChanged.AddListener((isSelected) => ToggleMaster(masterId, isSelected));
 
-                //         // Toggle 버튼의 라벨에 캐릭터 이름 설정
-                //         Transform labelTransform = toggleButtonTransform.Find("Label");
-                //         if (labelTransform != null)
-                //         {
-                //             Text labelText = labelTransform.GetComponent<Text>();
-                //             if (labelText != null)
-                //             {
-                //                 labelText.text = masterDatas[i].master_Id.ToString(); // 캐릭터 이름 설정
-                //             }
-                //         }
-                //     }
-                // }
-                // else
-                // {
-                //     Debug.LogError("Portrait or ToggleButton not found in masterPrefab");
-                // }
+                // Toggle 버튼의 라벨에 캐릭터 이름 설정
+                Transform labelTransform = portraitTransform.Find("Label");
+
+                if (labelTransform == null)
+                    continue;
+
+                if (!labelTransform.TryGetComponent<Text>(out var labelText))
+                    continue;
+
+                labelText.text = masterId.ToString(); // 캐릭터 이름 설정
             }
+        }
 
-            // Update is called once per frame
-            void Update()
-            {
+        // 마스터의 상태를 토글하는 메서드
+        void ToggleMaster(int masterId, bool isSelected)
+        {
+            if (isSelected == false)
+                return;
 
-            }
+            selectedMasterId = masterId;
+            Debug.Log("Selected Master: " + selectedMasterId);
+        }
 
-            // 마스터의 상태를 토글하는 메서드
-            void ToggleMaster(int masterId, bool isSelected)
-            {
-                if (isSelected)
-                {
-                    selectedMasterId = masterId;
-                    Debug.Log("Selected Master: " + selectedMasterId);
-                }
-            }
+        public void NextScene()
+        {
+            if (selectedMasterId == 0)
+                return;
+
+            // 선택된 MasterId를 PlayerPrefs에 저장
+            PlayerPrefs.SetInt("SelectedMasterId", selectedMasterId);
+            PlayerPrefs.Save();
+
+            nextScene.Play();
         }
     }
 }
