f399521301ef3f11aa1a635d6d646e8cd8682e1c
Milkbboy <okayly@hotmail.com>
2024-10-29 15:25:44 +0900

    빌드 에러 수정

--- Assets/ERang/Scripts/Ability/IAbility.cs
+++ Assets/ERang/Scripts/Ability/IAbility.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using ERang.Data;
-using UnityEngine;
 
 namespace ERang
 {
     public interface IAbility
     {
+        public AbilityType AbilityType => AbilityType.None;
         public List<(bool isAffect, int slot, int cardId, CardType cardType, int before, int after, int changeValue)> Changes { get; set; }
 
         IEnumerator Apply(AiData aiData, AbilityData abilityData, BoardSlot selfSlot, List<BoardSlot> targetSlots);
