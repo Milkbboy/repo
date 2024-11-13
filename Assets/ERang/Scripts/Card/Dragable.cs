abe3953405e3b7f36f7eb7a11ef79e4afaab53d1
Milkbboy <okayly@hotmail.com>
2024-11-13 16:36:35 +0900

    배틀 - 드래그 중인 카드 제일 앞에 오게 설정

--- Assets/ERang/Scripts/Card/Dragable.cs
+++ Assets/ERang/Scripts/Card/Dragable.cs
@@ -10,22 +10,43 @@ namespace ERang
         public bool IsDragging => isDragging;
 
         private bool isDragging = false;
+        private int originalSortingOrder;
+        private Renderer[] renderers;
+
+        void Awake()
+        {
+            renderers = GetComponentsInChildren<Renderer>();
+        }
 
         void OnMouseDown()
         {
             isDragging = true;
+
+            // 모든 렌더러의 sortingOrder를 높게 설정
+            foreach (var renderer in renderers)
+            {
+                originalSortingOrder = renderer.sortingOrder;
+                renderer.sortingOrder = 1000; // 높은 값으로 설정하여 맨 앞으로 이동
+            }
         }
 
         void OnMouseDrag()
         {
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
             transform.position = objPosition;
         }
 
         void OnMouseUp()
         {
             isDragging = false;
+
+            // 모든 렌더러의 sortingOrder를 원래 값으로 복원
+            foreach (var renderer in renderers)
+            {
+                renderer.sortingOrder = originalSortingOrder;
+            }
         }
     }
 }
