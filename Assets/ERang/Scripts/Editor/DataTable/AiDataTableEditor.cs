9f794232d018465f2df1b911c8713a7f302dc22a
Milkbboy <okayly@hotmail.com>
2024-11-15 15:32:09 +0900

    테이블 데이터 인스펙터 출력 수정
    - 스크롤창 좀 키우고 id 출력되게 수정

---
+++ Assets/ERang/Scripts/Editor/DataTable/AiDataTableEditor.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+using ERang.Table;
+
+namespace ERang
+{
+    [CustomEditor(typeof(AiDataTable))]
+    public class AiDataTableEditor : Editor
+    {
+        private SerializedProperty itemsProperty;
+        private Vector2 scrollPosition;
+
+        private void OnEnable()
+        {
+            itemsProperty = serializedObject.FindProperty("items");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.LabelField("Ai Data Table", EditorStyles.boldLabel);
+
+            // 인스펙터 창의 높이를 동적으로 가져옵니다.
+            Rect lastRect = GUILayoutUtility.GetLastRect();
+            float inspectorHeight = Screen.height - lastRect.y - 100; // 상단 레이블과 여백을 고려하여 높이 조정
+
+            // 스크롤 뷰를 추가하여 리스트 창의 크기를 조정합니다.
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Height(inspectorHeight));
+            
+            EditorGUILayout.PropertyField(itemsProperty, new GUIContent("Items"), true);
+
+            EditorGUILayout.EndScrollView();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/ERang/Scripts/Editor/Entity/CardDataEntityDrawer.cs
@@ -0,0 +1,71 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace ERang
+{
+    [CustomPropertyDrawer(typeof(CardDataEntity))]
+    public class CardDataEntityDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Card_Id 값을 가져옵니다.
+            SerializedProperty cardIdProperty = property.FindPropertyRelative("Card_Id");
+            if (cardIdProperty == null)
+            {
+                EditorGUI.LabelField(position, "Card_Id: SerializedProperty is null");
+                EditorGUI.EndProperty();
+                return;
+            }
+            string cardId = cardIdProperty.intValue.ToString();
+
+            // Card_Id 값을 레이블로 사용합니다.
+            label = new GUIContent($"{cardId}");
+
+            // 텍스트 색상을 설정하기 위한 GUIStyle을 생성합니다.
+            GUIStyle labelStyle = new GUIStyle(EditorStyles.label);
+            if (ColorUtility.TryParseHtmlString("#dd9933", out Color labelColor))
+            {
+                labelStyle.normal.textColor = labelColor; // 원하는 색상으로 변경
+            }
+
+            // 기본 속성 필드를 그립니다.
+            Rect labelRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            EditorGUI.LabelField(labelRect, label, labelStyle);
+            position.y += EditorGUIUtility.singleLineHeight + 2;
+            EditorGUI.indentLevel = 0;
+
+            // 각 필드를 동적으로 가져와서 그립니다.
+            SerializedProperty iterator = property.Copy();
+            SerializedProperty endProperty = property.GetEndProperty();
+            iterator.NextVisible(true); // 첫 번째 자식 속성으로 이동
+
+            while (iterator.NextVisible(false) && !SerializedProperty.EqualContents(iterator, endProperty))
+            {
+                Rect fieldRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+                EditorGUI.PropertyField(fieldRect, iterator, true);
+                position.y += EditorGUIUtility.singleLineHeight + 2;
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            // 각 필드의 높이를 합산하여 반환합니다.
+            SerializedProperty iterator = property.Copy();
+            SerializedProperty endProperty = property.GetEndProperty();
+            iterator.NextVisible(true); // 첫 번째 자식 속성으로 이동
+
+            int fieldCount = 0;
+
+            while (iterator.NextVisible(false) && !SerializedProperty.EqualContents(iterator, endProperty))
+            {
+                fieldCount++;
+            }
+
+            return (fieldCount + 1) * (EditorGUIUtility.singleLineHeight + 2); // +1 for the Card_Id label
+        }
+    }
+}
